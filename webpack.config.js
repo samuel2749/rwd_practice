1b90ec87437be4251ea9ce9e17a63ca348256594
Samuel Lin <samuel2749@gmail.com>
2017-10-20 16:59:17 +0800

    Added the image and html of loader in webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,19 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 module.exports = {
     entry: {
         app: './src/js/index.js',
-        about: './src/js/about.js'
+        about: './src/js/about.js',
+        demo01: './src/js/demo01.js'
     },
     output: {
-        filename: '[name].bundle.js',
+        filename: 'js/[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     devtool: "inline-source-map",
     devServer: {
         contentBase: './dist'
     },
-    module:{
-        rules:[
-            {
+    module: {
+        rules: [{
                 test: /\.css$/,
                 /*use: [
                     'style-loader',
@@ -28,7 +28,7 @@ module.exports = {
                 ]*/
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: [ 'css-loader', 'postcss-loader' ]
+                    use: ['css-loader', 'postcss-loader']
                 })
             },
             {
@@ -41,10 +41,24 @@ module.exports = {
                 ]*/
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: [
-                        {loader: 'css-loader', options: {sourceMap: true}},
-                        {loader: 'postcss-loader', options: {sourceMap: true}},
-                        {loader: 'sass-loader', options: {sourceMap: true}},
+                    use: [{
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
                     ]
                 })
             },
@@ -52,12 +66,50 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [{
+                        loader: 'url-loader',
+                        options: {
+                            limit: 2048,
+                            name: "images/[name].[ext]?[hash:10]",
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            optipng: {
+                                optimizationLevel: 7,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // Specifying webp here will create a WEBP version of your JPG/PNG images
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
-        new ExtractTextPlugin('[name].css'),
+        new ExtractTextPlugin('css/[name].css'),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'src/views/index.html',
@@ -68,22 +120,27 @@ module.exports = {
             template: 'src/views/about.html',
             chunks: ['about']
         }),
+        new HtmlWebpackPlugin({
+            filename: 'demo01.html',
+            template: 'src/views/demo01.html',
+            chunks: ['demo01']
+        }),
         new BrowserSyncPlugin(
             // BrowserSync options
             {
-              // browse to http://localhost:3000/ during development
-              host: 'localhost',
-              port: 3000,
-              // proxy the Webpack Dev Server endpoint
-              // (which should be serving on http://localhost:3100/)
-              // through BrowserSync
-              proxy: 'http://localhost:8080/'
+                // browse to http://localhost:3000/ during development
+                host: 'localhost',
+                port: 3000,
+                // proxy the Webpack Dev Server endpoint
+                // (which should be serving on http://localhost:3100/)
+                // through BrowserSync
+                proxy: 'http://localhost:8080/'
             },
             // plugin options
             {
-              // prevent BrowserSync from reloading the page
-              // and let Webpack Dev Server take care of this
-              reload: false
+                // prevent BrowserSync from reloading the page
+                // and let Webpack Dev Server take care of this
+                reload: false
             }
         )
     ]
